1124953d2caf939001bbbef7c87ffcff5cc0e5c2
lyang36 <lyang.thu@gmail.com>
2018-08-17 15:03:53 -0400

    replaces tabs with spaces, contract not tested ..

--- AtomicSwap.sol
+++ AtomicSwap.sol
@@ -22,27 +22,27 @@ contract ERC20 {
 
 
 contract ERC721{
-	event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
-	event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
-	event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
-	function balanceOf(address _owner) external view returns (uint256);
-	function ownerOf(uint256 _tokenId) external view returns (address);
-	function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes data) external payable;
-	function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable;
-	function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
-	function approve(address _approved, uint256 _tokenId) external payable;
-	function setApprovalForAll(address _operator, bool _approved) external;
-	function getApproved(uint256 _tokenId) external view returns (address);
-	function isApprovedForAll(address _owner, address _operator) external view returns (bool);
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+    event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+    function balanceOf(address _owner) external view returns (uint256);
+    function ownerOf(uint256 _tokenId) external view returns (address);
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes data) external payable;
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable;
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
+    function approve(address _approved, uint256 _tokenId) external payable;
+    function setApprovalForAll(address _operator, bool _approved) external;
+    function getApproved(uint256 _tokenId) external view returns (address);
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
 }
 
 contract AtomicSwap {
 
-	/*The state of the swap instance*/
+    /*The state of the swap instance*/
     enum State { Empty, Initiator}
 
-	/*The AssetType of the swap instance*/
-	enum AssetType { Native, ERC20Value, ERC721Value }
+    /*The AssetType of the swap instance*/
+    enum AssetType { Native, ERC20Value, ERC721Value }
 
     struct Swap {
         uint initTimestamp;
@@ -54,202 +54,202 @@ contract AtomicSwap {
         address initiator;
         address participant;
 
-		address tokenAddress;
+        address tokenAddress;
 
         uint256 value;
 
         bool emptied;
 
         State state;
-		AssetType assetType;
+        AssetType assetType;
     }
 
     mapping(bytes20 => Swap) public swaps;
     
-	event Refunded(uint _refundTime);
+    event Refunded(uint _refundTime);
     event Redeemed(uint _redeemTime);
 
-	event Initiated(
-		uint _initTimestamp,
-    	uint _refundTime,
-    	bytes20 _hashedSecret,
-    	address _participant,
-    	address _initiator,
+    event Initiated(
+        uint _initTimestamp,
+        uint _refundTime,
+        bytes20 _hashedSecret,
+        address _participant,
+        address _initiator,
 
-		AssetType _assetType,
+        AssetType _assetType,
 
-		uint256 _funds,
+        uint256 _funds,
 
-		address _tokenAddress
-	);
+        address _tokenAddress
+    );
 
-	constructor() public {}
+    constructor() public {}
     
-	modifier isRefundable(bytes20 _hashedSecret) {
-	    require(block.timestamp > swaps[_hashedSecret].initTimestamp + swaps[_hashedSecret].refundTime);
-	    require(swaps[_hashedSecret].emptied == false);
-	    _;
-	}
-	
-	modifier isRedeemable(bytes20 _hashedSecret, bytes32 _secret) {
-	    require(ripemd160(_secret) == _hashedSecret);
-		require(block.timestamp < swaps[_hashedSecret].initTimestamp + swaps[_hashedSecret].refundTime);
-	    require(swaps[_hashedSecret].emptied == false);
-	    _;
-	}
-	
-	modifier isInitiator(bytes20 _hashedSecret) {
-	    require(msg.sender == swaps[_hashedSecret].initiator);
-	    _;
-	}
-	
-	modifier isNotInitiated(bytes20 _hashedSecret) {
-	    require(swaps[_hashedSecret].state == State.Empty);
-	    _;
-	}
+    modifier isRefundable(bytes20 _hashedSecret) {
+        require(block.timestamp > swaps[_hashedSecret].initTimestamp + swaps[_hashedSecret].refundTime);
+        require(swaps[_hashedSecret].emptied == false);
+        _;
+    }
+    
+    modifier isRedeemable(bytes20 _hashedSecret, bytes32 _secret) {
+        require(ripemd160(_secret) == _hashedSecret);
+        require(block.timestamp < swaps[_hashedSecret].initTimestamp + swaps[_hashedSecret].refundTime);
+        require(swaps[_hashedSecret].emptied == false);
+        _;
+    }
+    
+    modifier isInitiator(bytes20 _hashedSecret) {
+        require(msg.sender == swaps[_hashedSecret].initiator);
+        _;
+    }
+    
+    modifier isNotInitiated(bytes20 _hashedSecret) {
+        require(swaps[_hashedSecret].state == State.Empty);
+        _;
+    }
 
 
-	function __initiate (uint _refundTime,bytes20 _hashedSecret,address _participant) 
-		private
-	{
-	    swaps[_hashedSecret].refundTime = _refundTime;
-	    swaps[_hashedSecret].initTimestamp = block.timestamp;
-	    swaps[_hashedSecret].hashedSecret = _hashedSecret;
-	    swaps[_hashedSecret].participant = _participant;
-	    swaps[_hashedSecret].initiator = msg.sender;
+    function __initiate (uint _refundTime,bytes20 _hashedSecret,address _participant) 
+        private
+    {
+        swaps[_hashedSecret].refundTime = _refundTime;
+        swaps[_hashedSecret].initTimestamp = block.timestamp;
+        swaps[_hashedSecret].hashedSecret = _hashedSecret;
+        swaps[_hashedSecret].participant = _participant;
+        swaps[_hashedSecret].initiator = msg.sender;
         swaps[_hashedSecret].state = State.Initiator;
-	}
+    }
 
-	/**
-		Initiate the swapper with the native token.
-	*/
-	function initiate (uint _refundTime,bytes20 _hashedSecret,address _participant) 
-		public
-	    payable 
-	    isNotInitiated(_hashedSecret)   
-	{
-		__initiate(_refundTime, _hashedSecret, _participant);
+    /**
+        Initiate the swapper with the native token.
+    */
+    function initiate (uint _refundTime,bytes20 _hashedSecret,address _participant) 
+        public
+        payable 
+        isNotInitiated(_hashedSecret)   
+    {
+        __initiate(_refundTime, _hashedSecret, _participant);
 
         swaps[_hashedSecret].value = msg.value;
-		swaps[_hashedSecret].assetType = AssetType.Native;
+        swaps[_hashedSecret].assetType = AssetType.Native;
 
-		emit Initiated(
-			swaps[_hashedSecret].initTimestamp,
-    		_refundTime,
-    		_hashedSecret,
-    		_participant,
-    		msg.sender,
-			AssetType.Native,
-		 	msg.value,
-			address(0)
-		);
-	}
+        emit Initiated(
+            swaps[_hashedSecret].initTimestamp,
+            _refundTime,
+            _hashedSecret,
+            _participant,
+            msg.sender,
+            AssetType.Native,
+             msg.value,
+            address(0)
+        );
+    }
 
-	/**
-		Initiate the swapper with the ERC20Value token.
-	*/
-	function initiateERC20 (uint _refundTime, 
-							bytes20 _hashedSecret, 
-							address _participant, 
-							address _tokenAddress, 
-							uint256 _value) 
-		public
-	    isNotInitiated(_hashedSecret)    
-	{
-		__initiate(_refundTime, _hashedSecret, _participant);
+    /**
+        Initiate the swapper with the ERC20Value token.
+    */
+    function initiateERC20 (uint _refundTime, 
+                            bytes20 _hashedSecret, 
+                            address _participant, 
+                            address _tokenAddress, 
+                            uint256 _value) 
+        public
+        isNotInitiated(_hashedSecret)    
+    {
+        __initiate(_refundTime, _hashedSecret, _participant);
 
-		require(ERC20(_tokenAddress).transferFrom(msg.sender, this, _value) == true);
+        require(ERC20(_tokenAddress).transferFrom(msg.sender, this, _value) == true);
 
         swaps[_hashedSecret].value = _value;
-		swaps[_hashedSecret].assetType = AssetType.ERC20Value;
-		swaps[_hashedSecret].tokenAddress = _tokenAddress;
+        swaps[_hashedSecret].assetType = AssetType.ERC20Value;
+        swaps[_hashedSecret].tokenAddress = _tokenAddress;
 
-		emit Initiated(
-			swaps[_hashedSecret].initTimestamp,
-    		_refundTime,
-    		_hashedSecret,
-    		_participant,
-    		msg.sender,
-			AssetType.ERC20Value,
-		 	_value,
-			_tokenAddress
-		);
-	}
+        emit Initiated(
+            swaps[_hashedSecret].initTimestamp,
+            _refundTime,
+            _hashedSecret,
+            _participant,
+            msg.sender,
+            AssetType.ERC20Value,
+             _value,
+            _tokenAddress
+        );
+    }
 
-	/**
-		Initiate the swapper with the ERC721Value token.
-	*/
-	function initiateERC721 (uint _refundTime, 
-							bytes20 _hashedSecret, 
-							address _participant, 
-							address _tokenAddress, 
-							uint256 _tokenId) 
-		public
-	    isNotInitiated(_hashedSecret)    
-	{
-		__initiate(_refundTime, _hashedSecret, _participant);
+    /**
+        Initiate the swapper with the ERC721Value token.
+    */
+    function initiateERC721 (uint _refundTime, 
+                            bytes20 _hashedSecret, 
+                            address _participant, 
+                            address _tokenAddress, 
+                            uint256 _tokenId) 
+        public
+        isNotInitiated(_hashedSecret)    
+    {
+        __initiate(_refundTime, _hashedSecret, _participant);
 
-		ERC721(_tokenAddress).transferFrom(msg.sender, this, _tokenId);
+        ERC721(_tokenAddress).transferFrom(msg.sender, this, _tokenId);
 
         swaps[_hashedSecret].value = _tokenId;
-		swaps[_hashedSecret].assetType = AssetType.ERC721Value;
-		swaps[_hashedSecret].tokenAddress = _tokenAddress;
+        swaps[_hashedSecret].assetType = AssetType.ERC721Value;
+        swaps[_hashedSecret].tokenAddress = _tokenAddress;
 
-		emit Initiated(
-			swaps[_hashedSecret].initTimestamp,
-    		_refundTime,
-    		_hashedSecret,
-    		_participant,
-    		msg.sender,
-			AssetType.ERC721Value,
-		 	_tokenId,
-			_tokenAddress
-		);
-	}
+        emit Initiated(
+            swaps[_hashedSecret].initTimestamp,
+            _refundTime,
+            _hashedSecret,
+            _participant,
+            msg.sender,
+            AssetType.ERC721Value,
+             _tokenId,
+            _tokenAddress
+        );
+    }
 
 
-	function redeem(bytes32 _secret, bytes20 _hashedSecret) 
-		public
-	    isRedeemable(_hashedSecret, _secret)
-	{
+    function redeem(bytes32 _secret, bytes20 _hashedSecret) 
+        public
+        isRedeemable(_hashedSecret, _secret)
+    {
 
         if(swaps[_hashedSecret].state == State.Initiator){
-			if(swaps[_hashedSecret].assetType == AssetType.Native){
-            	swaps[_hashedSecret].participant.transfer(swaps[_hashedSecret].value);
-			}
-			if(swaps[_hashedSecret].assetType == AssetType.ERC20Value){
-            	ERC20(swaps[_hashedSecret].tokenAddress).transferFrom(this, swaps[_hashedSecret].participant, swaps[_hashedSecret].value);
-			}
+            if(swaps[_hashedSecret].assetType == AssetType.Native){
+                swaps[_hashedSecret].participant.transfer(swaps[_hashedSecret].value);
+            }
+            if(swaps[_hashedSecret].assetType == AssetType.ERC20Value){
+                ERC20(swaps[_hashedSecret].tokenAddress).transferFrom(this, swaps[_hashedSecret].participant, swaps[_hashedSecret].value);
+            }
 
-			if(swaps[_hashedSecret].assetType == AssetType.ERC721Value){
-            	ERC721(swaps[_hashedSecret].tokenAddress).transferFrom(this, swaps[_hashedSecret].participant, swaps[_hashedSecret].value);
-			}				
+            if(swaps[_hashedSecret].assetType == AssetType.ERC721Value){
+                ERC721(swaps[_hashedSecret].tokenAddress).transferFrom(this, swaps[_hashedSecret].participant, swaps[_hashedSecret].value);
+            }                
 
         }
         swaps[_hashedSecret].emptied = true;
         emit Redeemed(block.timestamp);
         swaps[_hashedSecret].secret = _secret;
-	}
+    }
 
-	function refund(bytes20 _hashedSecret)
-		public
-	    isRefundable(_hashedSecret) 
-	{
+    function refund(bytes20 _hashedSecret)
+        public
+        isRefundable(_hashedSecret) 
+    {
 
         if(swaps[_hashedSecret].state == State.Initiator){
-			if(swaps[_hashedSecret].assetType == AssetType.Native){
-            	swaps[_hashedSecret].initiator.transfer(swaps[_hashedSecret].value);
-			}
+            if(swaps[_hashedSecret].assetType == AssetType.Native){
+                swaps[_hashedSecret].initiator.transfer(swaps[_hashedSecret].value);
+            }
 
-			if(swaps[_hashedSecret].assetType == AssetType.ERC20Value){
-            	ERC20(swaps[_hashedSecret].tokenAddress).transferFrom(this, swaps[_hashedSecret].initiator, swaps[_hashedSecret].value);
-			}
+            if(swaps[_hashedSecret].assetType == AssetType.ERC20Value){
+                ERC20(swaps[_hashedSecret].tokenAddress).transferFrom(this, swaps[_hashedSecret].initiator, swaps[_hashedSecret].value);
+            }
 
-			if(swaps[_hashedSecret].assetType == AssetType.ERC721Value){
-            	ERC721(swaps[_hashedSecret].tokenAddress).transferFrom(this, swaps[_hashedSecret].initiator, swaps[_hashedSecret].value);
-			}
+            if(swaps[_hashedSecret].assetType == AssetType.ERC721Value){
+                ERC721(swaps[_hashedSecret].tokenAddress).transferFrom(this, swaps[_hashedSecret].initiator, swaps[_hashedSecret].value);
+            }
         }
         swaps[_hashedSecret].emptied = true;
-	    emit Refunded(block.timestamp);
-	}
+        emit Refunded(block.timestamp);
+    }
 }
